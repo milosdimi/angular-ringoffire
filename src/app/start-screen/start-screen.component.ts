6926bc0095c9a982ddf4b45aa6f4309e0c011286
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-12-01 13:52:52 +0100

    03 - Spiel starten

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
   imports: [],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  newGame() {
+    // Start a new game by navigating to the game route
+    this.router.navigateByUrl('/game');
+  }
 }
