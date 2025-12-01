431ff115489d316a5c64b6b9c89e606cfa8d7b8d
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-12-01 15:07:20 +0100

    ngFor error

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // hier ist NgFor, NgIf, NgStyle usw. drin
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
-
+  // dein Code
 }
