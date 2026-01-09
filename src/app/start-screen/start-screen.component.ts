9ea46c28f618add7d877a33a32334377239b6157
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-01-09 01:24:57 +0100

    start-screen responsive

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss',
 })
 export class StartScreenComponent {
   constructor(private router: Router) {}
 
+  isStarting = false;
+
   newGame() {
-    // Start a new game by navigating to the game route
+    if (this.isStarting) return;
+    this.isStarting = true;
+
     this.router.navigateByUrl('/game');
   }
 }
